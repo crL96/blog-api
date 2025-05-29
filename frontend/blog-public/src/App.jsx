99f3b2189139013c9abde03a8af9382605f6b5b3
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-29 11:45:59 +0200

    Add env var for api_url and use it to fetch blog posts from api

--- frontend/blog-public/src/App.jsx
+++ frontend/blog-public/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL + "/posts")
+      .then((response) => response.json())
+      .then((data) => setBlogPosts(data))
+  }, [])
 
   return (
     <>
       <h1>Hello World!</h1>
+      {blogPosts.map((post) => {
+        return (
+          <div key={post.id}>{post.text}</div>
+        );
+      })}
     </>
   )
 }
