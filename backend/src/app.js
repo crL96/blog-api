05a7ef63b0b6f47711f771b5ea5d57c2bbf633de
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-29 11:25:59 +0200

    Install and setup cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const postsRouter = require("./routes/posts");
 const authRouter = require("./routes/auth");
+const cors = require("cors");
 
 const app = express();
 
 // App Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
