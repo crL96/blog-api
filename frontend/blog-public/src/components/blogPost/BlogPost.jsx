eb9b38f060f65968d1c26c1693c25a72f201a41a
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-02 13:38:39 +0200

    Add submitComment component and integrate it in each Blogpost

--- frontend/blog-public/src/components/blogPost/BlogPost.jsx
+++ frontend/blog-public/src/components/blogPost/BlogPost.jsx
@@ -2,15 +2,25 @@ import styles from "./blogPost.module.css";
 import { useState, useEffect } from "react";
 const API_URL = import.meta.env.VITE_API_URL;
 import Comment from "../comment/Comment";
+import SubmitComment from "../submitComment/SubmitComment";
 
 function BlogPost({ post }) {
     const [comments, setComments] = useState([]);
+    const [showSubmitComment, setShowSubmitComment] = useState(false);
     
     useEffect(() => {
         fetch(`${API_URL}/posts/${post.id}/comments`)
             .then((response) => response.json())
             .then((data) => setComments(data))
     }, [post.id])
+
+    function toggleShowSubmitComment() {
+        if (showSubmitComment) {
+            setShowSubmitComment(false);
+        } else {
+            setShowSubmitComment(true);
+        }
+    }
 
     return (
         <div className={[styles.blogPost, "blogPost"].join(" ")}>
@@ -21,7 +31,15 @@ function BlogPost({ post }) {
                 <p className={styles.timestamp}>{post.timestamp.toString().split("T")[0]}</p>
             </div>
             <div className={styles.comments}>
-                <h3>Comments</h3>
+                <div className={styles.commentsHeader}>
+                    <h3>Comments</h3>
+                    <button onClick={toggleShowSubmitComment}>
+                        {showSubmitComment ? ("Hide") : ("Add a comment")}
+                    </button>
+                </div>
+                {showSubmitComment ? (
+                    <SubmitComment postId={post.id}/>
+                ) : null}
                 {comments.map((comment) => {
                     return (
                         <Comment key={comment.id} comment={comment} />
---
+++ frontend/blog-public/src/components/submitComment/SubmitComment.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import styles from "./submitComment.module.css";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function SubmitComment({ postId }) {
+    const [errorMessages, setErrorMessages] = useState([]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const comment = Object.fromEntries(formData);
+
+        try {
+            const response = await fetch(`${API_URL}/posts/${postId}/comments`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "Application/JSON",
+                },
+                body: JSON.stringify(comment),
+            });
+            if (response.status === 200) {
+                setErrorMessages([]);
+                e.target.reset()
+            } else if (response.status === 400) {
+                const responseObj = await response.json()
+                console.log(responseObj);
+                setErrorMessages(responseObj.errors)
+            } else {
+                console.log(response);
+                setErrorMessages[{ msg: "Something went wrong, please try again" }]
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className={styles.div}>
+            <h3 className={styles.header}>Add Comment</h3>
+            {errorMessages.length !== 0 ? (
+                <ul>
+                    {errorMessages.map((error, index) => {
+                        return (
+                            <li key={index}>{error.msg}</li>
+                        );
+                    })}
+                </ul>
+            ): null }
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label htmlFor="author">Name: </label>
+                <input type="text" name="author" id="author" />
+                <label htmlFor="message">Comment: </label>
+                <textarea name="message" id="message" rows="4"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default SubmitComment;
