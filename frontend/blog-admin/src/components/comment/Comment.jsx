d71fc88eecf21a833babe18a385e50e149ce3e06
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-02 16:57:03 +0200

    Add functionality to delete comments

--- frontend/blog-admin/src/components/comment/Comment.jsx
+++ frontend/blog-admin/src/components/comment/Comment.jsx
@@ -1,12 +1,32 @@
 import styles from "./comment.module.css";
+const API_URL = import.meta.env.VITE_API_URL;
+import { useNavigate } from "react-router-dom";
 
 function Comment({ comment }) {
+    const navigate = useNavigate();
+
+    async function handleDelete() {
+        try {
+            const res = await fetch(`${API_URL}/posts/${comment.postId}/comments/${comment.id}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: sessionStorage.getItem("token")
+                }
+            });
+            if (res.status === 401) {
+                navigate("login");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
     
     return (
         <div className={[styles.comment, "comment"].join(" ")}>
             <h4>{comment.author}</h4>
             <p>{comment.message}</p>
             <p>{comment.timestamp.toString().split("T")[0]}</p>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     );
 }
