01a4c866e821c861da856ce9447e1150a01c90fb
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-02 15:25:31 +0200

    Add env var for api_url and fetch all blog posts

--- frontend/blog-admin/src/App.jsx
+++ frontend/blog-admin/src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
+import { useState, useEffect } from "react";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL + "/posts/admin")
+      .then((response) => response.json())
+      .then((data) => setBlogPosts(data))
+  }, [])
+
+  console.log(blogPosts);
+
+  if (blogPosts.length === 0) return <p>Loading...</p>
 
   return (
     <>
