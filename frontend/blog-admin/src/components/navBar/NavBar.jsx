3e62fd36f223924eedd2f703187721feaac3c20d
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-02 16:43:46 +0200

    Sort navBar layout

--- frontend/blog-admin/src/components/navBar/NavBar.jsx
+++ frontend/blog-admin/src/components/navBar/NavBar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from "./navBar.module.css";
 
 function NavBar() {
   return (
-    <nav>
+    <nav className={styles.navBar}>
         <ul>
           <li>
             <Link to="/">Blog Posts</Link>
+          </li>
+          <li>
             <Link to="login">Log In</Link>
           </li>
         </ul>
