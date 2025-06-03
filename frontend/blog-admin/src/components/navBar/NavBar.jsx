0f4c57ba0f635aecb33530d8ec672464b5d6cf79
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-03 11:41:43 +0200

    Navbar keeps track if user is logged in and adjusts available routes

--- frontend/blog-admin/src/components/navBar/NavBar.jsx
+++ frontend/blog-admin/src/components/navBar/NavBar.jsx
@@ -1,22 +1,47 @@
 import { Link } from "react-router-dom";
 import styles from "./navBar.module.css";
+import { useState, useEffect } from "react";
 
 function NavBar() {
+  const [signedIn, setSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      setSignedIn(true);
+    } else {
+      setSignedIn(false);
+    }
+  }, [])
+
+  function handleLogout() {
+    sessionStorage.removeItem("token");
+  }
+
   return (
     <nav className={styles.navBar}>
         <ul>
-          <li>
-            <Link to="/">Blog Posts</Link>
-          </li>
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/newpost">New post</Link>
-          </li>
+          {signedIn ? ( // If user is signed in
+            <>
+              <li>
+                <Link to="/">Blog Posts</Link>
+              </li>
+              <li>
+                <Link to="/newpost">New Post</Link>
+              </li>
+              <li>
+                <Link to="/login" onClick={handleLogout}>Log Out</Link>
+              </li>
+            </>
+          ) : ( // If user is not signed in
+            <>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            </>
+          )}
         </ul>
     </nav>
   );
